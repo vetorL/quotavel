8815fc6f2813e5f9ee6053b7847a13dba0db9657
vetorL <145482274+vetorL@users.noreply.github.com>
2025-05-01 14:21:18 -0300

    Adiciona efeito de digitação no título do site

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
+"use client";
+
+import Typewriter from "react-ts-typewriter";
+
 export default function Home() {
   return (
     <div className="flex items-center justify-center min-h-screen">
-      <h1 className="text-9xl font-bold italic">Quotavel</h1>
+      <h1 className="text-9xl font-bold italic">
+        <Typewriter
+          text="Quotavel"
+          speed={300}
+          cursor={true}
+          onFinished={() => {}}
+        />
+      </h1>
     </div>
   );
 }
